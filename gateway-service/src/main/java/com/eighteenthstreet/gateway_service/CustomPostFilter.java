e3a9fb8efd3f285a836206f94ecbe3aa6e70a1cf
Yeonnnny <kdoy030@gmail.com>
2025-03-14 16:12:21 +0900

    feat : gateway-service 추가

---
+++ gateway-service/src/main/java/com/eighteenthstreet/gateway_service/CustomPostFilter.java
@@ -0,0 +1,26 @@
+package com.eighteenthstreet.gateway_service;
+
+import java.util.logging.Logger;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+public class CustomPostFilter implements GlobalFilter, Ordered {
+	private static final Logger logger = Logger.getLogger(CustomPostFilter.class.getName());
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+			ServerHttpResponse response = exchange.getResponse();
+			logger.info("Post Filter : Response status code :"+response.getStatusCode());
+		}));
+	}
+
+	@Override
+	public int getOrder() { return Ordered.LOWEST_PRECEDENCE; }
+}
