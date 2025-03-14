f0324fbaf071e505abe935d9e11569d652ef33f2
kizzis <iswater.kim@gmail.com>
2025-03-14 11:32:31 +0900

    refactor: 기본 생성자 어노테이션 추가

--- common/src/main/java/base/BaseEntity.java
+++ common/src/main/java/base/BaseEntity.java
@@ -1,43 +1,47 @@
 package base;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class BaseEntity {
-    @CreatedBy
-    @Column(name = "created_by", updatable = false)
-    private String createdBy;
+	@CreatedBy
+	@Column(name = "created_by", updatable = false)
+	private String createdBy;
 
-    @CreatedDate
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
 
-    @LastModifiedBy
-    @Column(name = "modified_by")
-    private String modifiedBy;
+	@LastModifiedBy
+	@Column(name = "modified_by")
+	private String modifiedBy;
 
-    @LastModifiedDate
-    @Column(name = "modified_at")
-    private LocalDateTime modifiedAt;
-    
-    @Setter
-    @Column(name = "deleted_by")
-    private String deletedBy;
+	@LastModifiedDate
+	@Column(name = "modified_at")
+	private LocalDateTime modifiedAt;
 
-    @Setter
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
+	@Setter
+	@Column(name = "deleted_by")
+	private String deletedBy;
+
+	@Setter
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
 }
