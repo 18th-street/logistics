a392cf4efcfaedbc8552c1e64bd38c43a7bd1693
kizzis <iswater.kim@gmail.com>
2025-03-14 12:14:21 +0900

    refactor: 패키지 구조 변경

--- eureka-server/src/main/java/com/eighteenthstreet/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eighteenthstreet/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package eighteenthstreet.eureka_server;
+package com.eighteenthstreet.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/eighteenthstreet/eureka_server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/eighteenthstreet/eureka_server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package eighteenthstreet.eureka_server;
+package com.eighteenthstreet.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
