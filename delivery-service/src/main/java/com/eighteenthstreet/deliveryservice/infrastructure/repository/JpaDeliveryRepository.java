2d6064e458feefbb427724ca5315c5e01666f23b
Jong-Min <codejomo99@gmail.com>
2025-03-15 02:53:12 +0900

    feat: soft delete 기능 구현

--- delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/infrastructure/repository/JpaDeliveryRepository.java
+++ delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/infrastructure/repository/JpaDeliveryRepository.java
@@ -3,9 +3,17 @@ package com.eighteenthstreet.deliveryservice.infrastructure.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.eighteenthstreet.deliveryservice.domain.model.Delivery;
 import com.eighteenthstreet.deliveryservice.domain.repository.DeliveryRepository;
 
 public interface JpaDeliveryRepository extends JpaRepository<Delivery, UUID>, DeliveryRepository {
+
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE p_delivery SET deleted_at = current_timestamp "
+		+ "WHERE delivery_id = :deliveryId")
+	void softDeleteDelivery(@Param("deliveryId") UUID deliveryId);
 }
