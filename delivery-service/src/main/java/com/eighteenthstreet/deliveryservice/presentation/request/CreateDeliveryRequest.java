da68f5d08260f637095c1d77d8db9699c0337b07
Jong-Min <codejomo99@gmail.com>
2025-03-14 23:46:16 +0900

    feat: createDelivery DTO 생성

---
+++ delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/presentation/request/CreateDeliveryRequest.java
@@ -0,0 +1,23 @@
+package com.eighteenthstreet.deliveryservice.presentation.request;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class CreateDeliveryRequest {
+	private UUID orderId;
+	private UUID startHubId;
+	private UUID endHubId;
+	private String recipient;
+	private String destinationAddress;
+	private String recipientSlackId;
+}
