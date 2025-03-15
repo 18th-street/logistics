92a7e3fda6945665b17741a7072034e97b7eb0b3
Jong-Min <codejomo99@gmail.com>
2025-03-15 17:43:36 +0900

    feat: Autditing 을 위한 어노테이션

--- delivery-agent-service/src/main/java/com/eighteenthstreet/deliveryagentservice/DeliveryAgentServiceApplication.java
+++ delivery-agent-service/src/main/java/com/eighteenthstreet/deliveryagentservice/DeliveryAgentServiceApplication.java
@@ -2,13 +2,15 @@ package com.eighteenthstreet.deliveryagentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DeliveryAgentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryAgentServiceApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAgentServiceApplication.class, args);
 
-	}
+    }
 
 }
