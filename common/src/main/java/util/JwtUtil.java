55bca25c53747930b400a4b188ec830dd7d111d7
Yeonnnny <kdoy030@gmail.com>
2025-03-16 19:51:03 +0900

    feat: 공통 모듈에 JwtConfig 추가 및 SpringBootApplication에 패키지 명 추가

---
+++ common/src/main/java/util/JwtUtil.java
@@ -0,0 +1,50 @@
+package util;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Configuration
+public class JwtUtil {
+	@Value("${service.jwt.secret-key}")
+	private String secretKey;
+
+	public String getUserIdFromToken(String token) {
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+		Claims claims = Jwts.parser()
+			.verifyWith(key)
+			.build()
+			.parseSignedClaims(token)
+			.getBody();
+
+		return claims.get("userId", String.class);
+	}
+
+	public String getRoleFromToken(String token) {
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+		Claims claims = Jwts.parser()
+			.verifyWith(key)
+			.build()
+			.parseSignedClaims(token)
+			.getBody();
+
+		return claims.get("role", String.class);
+	}
+
+	public String getUsernameFromToken(String token) {
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+		Claims claims = Jwts.parser()
+			.verifyWith(key)
+			.build()
+			.parseSignedClaims(token)
+			.getBody();
+
+		return claims.get("username", String.class);
+	}
+}
--- gateway-service/src/main/java/com/eighteenthstreet/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/eighteenthstreet/gateway_service/GatewayServiceApplication.java
@@ -3,7 +3,7 @@ package com.eighteenthstreet.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"exception", "base", "com.eighteenthstreet.gateway_service"})
+@SpringBootApplication(scanBasePackages = {"exception", "base", "util", "com.eighteenthstreet.gateway_service"})
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
