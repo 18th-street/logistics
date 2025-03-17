00c5db7c7c0da0f5f704a2c7c03a9376476be460
Yeonnnny <kdoy030@gmail.com>
2025-03-18 01:35:58 +0900

    feat : 슬랙 관련 에러 코드 추가

--- common/src/main/java/exception/ErrorCode.java
+++ common/src/main/java/exception/ErrorCode.java
@@ -14,7 +14,10 @@ public enum ErrorCode {
 	FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근이 금지되었습니다.", "4003"),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", "4004"),
 
-	SLACK_SEND_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "슬랙 메시지 전송이 실패했습니다.", "S001");
+	SLACK_SEND_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "슬랙 메시지 전송을 실패했습니다.", "S001"),
+	EXTRACT_ID_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "슬랙 멤버 ID 추출을 실패했습니다.", "S002"),
+	SL_TRANSACTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "슬랙 메시지 저장을 실패했습니다.", "S003"),
+	SLACK_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 슬랙 메시지 정보를 찾을 수 없습니다.", "S004");
 
 	private final HttpStatus httpStatus;
 	private final String message;
