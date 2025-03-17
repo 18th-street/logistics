8707ca3e92c99efc7b7c36e7eb9e2afd61d9ab52
Yeonnnny <kdoy030@gmail.com>
2025-03-17 09:09:34 +0900

    refactor: DI를 활용하여 객체를 주입받아서 사용할 목적이므로 Component로 변경

--- common/src/main/java/util/JwtUtil.java
+++ common/src/main/java/util/JwtUtil.java
@@ -3,14 +3,14 @@ package util;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
-@Configuration
+@Component
 public class JwtUtil {
 	@Value("${service.jwt.secret-key}")
 	private String secretKey;
