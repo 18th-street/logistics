d6ef20eab596f68f2fa482f93beaa03730db3d35
Dominic.Yoon <808dominico@naver.com>
2025-03-18 02:59:23 +0900

    feat: Hub Service 생성

---
+++ hub-service/src/main/java/com/eighteenthstreet/hub_service/application/HubService.java
@@ -0,0 +1,16 @@
+package com.eighteenthstreet.hub_service.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.eighteenthstreet.hub_service.domain.HubRepository;
+import com.eighteenthstreet.hub_service.domain.model.Hub;
+import com.eighteenthstreet.hub_service.infrastructure.JpaHubRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class HubService {
+}
