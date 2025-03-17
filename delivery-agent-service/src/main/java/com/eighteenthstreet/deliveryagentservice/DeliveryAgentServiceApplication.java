25b07b758c40fca4bce23634b64d366d03c0f3ae
Jong-Min <codejomo99@gmail.com>
2025-03-17 15:52:22 +0900

    feat: common 공통모듈을 사용하기 위한 어노테이션 추가

--- delivery-agent-service/src/main/java/com/eighteenthstreet/deliveryagentservice/DeliveryAgentServiceApplication.java
+++ delivery-agent-service/src/main/java/com/eighteenthstreet/deliveryagentservice/DeliveryAgentServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"exception", "base", "com.eighteenthstreet.deliveryagentservice"})
 @EnableJpaAuditing
 public class DeliveryAgentServiceApplication {
 
--- delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/DeliveryServiceApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"exception", "base", "com.eighteenthstreet.deliveryservice"})
 @EnableJpaAuditing
 public class DeliveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceApplication.class, args);
+    }
 
 }
