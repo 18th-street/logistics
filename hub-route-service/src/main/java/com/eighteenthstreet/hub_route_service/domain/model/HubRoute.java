9f1b0502ae0e3c0ee3396a0f4a063c008213ed0c
808dominico@naver.com <808dominico@naver.com>
2025-03-18 12:33:58 +0900

    refactor: HubRoute 엔티티 수정

--- hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/domain/model/HubRoute.java
+++ hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/domain/model/HubRoute.java
@@ -2,12 +2,16 @@ package com.eighteenthstreet.hub_route_service.domain.model;
 
 import java.util.UUID;
 
+import com.eighteenthstreet.hub_service.domain.model.Hub;
+
 import base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class HubRoute extends BaseEntity {
@@ -16,18 +20,23 @@ public class HubRoute extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID hubRouteId;
 
+	@ManyToOne
+	@JoinColumn(name = "departure_hub_id", nullable = false)
+	private Hub departureHubId;
+
+	@ManyToOne
+	@JoinColumn(name = "arrival_hub_id", nullable = false)
+	private Hub arrivalHubId;
+
 	@Column(name = "estimated_distance")
-	private double estimatedDistance;
+	private Double estimatedDistance;
 
 	@Column(name = "estimated_duration")
-	private double estimatedDuration;
+	private Double estimatedDuration;
+
+	@Column(name = "hop_count") // 경유 허브 개수
+	private Integer hopCount;
 
 	@Column
 	private String status;
-
-	@Column(name = "departure_hub_id")
-	private UUID departureHubId;
-
-	@Column(name = "arrival_hub_id")
-	private UUID arrivalHubId;
 }
