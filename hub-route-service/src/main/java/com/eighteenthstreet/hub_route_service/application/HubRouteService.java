550587a9430aff77baa7f4b8348439f9687d5c04
808dominico@naver.com <808dominico@naver.com>
2025-03-18 12:35:06 +0900

    refactor: Hub Route Service HubRepository, RestTemplate 추가

--- hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/application/HubRouteService.java
+++ hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/application/HubRouteService.java
@@ -1,8 +1,10 @@
 package com.eighteenthstreet.hub_route_service.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.eighteenthstreet.hub_route_service.domain.HubRouteRepository;
+import com.eighteenthstreet.hub_service.domain.HubRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HubRouteService {
 
+	private final HubRepository hubRepository;
 	private final HubRouteRepository hubRouteRepository;
+	private final RestTemplate restTemplate;
 }
