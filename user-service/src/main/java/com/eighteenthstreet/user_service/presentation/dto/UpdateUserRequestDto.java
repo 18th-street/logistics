a1d81a6a17e0b303777ca3d9ac03e084340e0034
Yeonnnny <kdoy030@gmail.com>
2025-03-18 23:30:08 +0900

    refactor: Dto 수정

--- user-service/src/main/java/com/eighteenthstreet/user_service/presentation/dto/UpdateUserRequestDto.java
+++ user-service/src/main/java/com/eighteenthstreet/user_service/presentation/dto/UpdateUserRequestDto.java
@@ -14,16 +14,16 @@ public record UpdateUserRequestDto(
 	@Size(max = 50, message = "이메일은 최대 50자까지 입력할 수 있습니다.")
 	String email,
 
-	@NotBlank(message = "빈 값이 될 수 없습니다. 슬랙 아이디를 입력해주세요.")
-	@Size(max = 30, message = "슬랙 아이디는 30글자 이하로 구성되어야 합니다.")
-	String slackId,
-
 	@NotBlank(message = "빈 값이 될 수 없습니다. 이름을 입력해주세요.")
 	String name,
 
 	@NotBlank(message = "빈 값이 될 수 없습니다. 전화번호를 입력해주세요.")
 	@Pattern(regexp = "^\\d{10,11}$", message = "전화번호는 공백이나 기호 없이 10~11자리 숫자로 입력해주세요")
 	String phone,
+
+	@NotBlank(message = "빈 값이 될 수 없습니다. 슬랙 아이디를 입력해주세요.")
+	@Size(max = 30, message = "슬랙 아이디는 30글자 이하로 구성되어야 합니다.")
+	String slackId,
 	Role role,
 	Status status
 ) {
