b96f38896380744a65d5194f8d0f04d460553435
Jong-Min <codejomo99@gmail.com>
2025-03-20 18:24:19 +0900

    feat: [Delivery-Service] Delivery-Route 조회용 Client 및 DTO 추가

--- delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/application/client/DeliveryRouteClient.java
+++ delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/application/client/DeliveryRouteClient.java
@@ -1,15 +1,22 @@
 package com.eighteenthstreet.deliveryservice.application.client;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.eighteenthstreet.deliveryservice.application.dto.DeliveryRouteDto;
 
 @FeignClient(name = "delivery-route-service")
 public interface DeliveryRouteClient {
 	@DeleteMapping("/api/v1/delivery-routes/by-delivery/{deliveryId}")
 	ResponseEntity<Map<String, String>> deleteDeliveryRouteByDeliveryId(@PathVariable("deliveryId") UUID deliveryId);
+
+	@GetMapping("/api/v1/delivery-routes/by-delivery/{deliveryId}")
+	ResponseEntity<List<DeliveryRouteDto>> getDeliveryRoutesByDeliveryId(@PathVariable("deliveryId") UUID deliveryId);
 }
