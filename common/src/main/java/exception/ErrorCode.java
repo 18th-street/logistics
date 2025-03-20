51a6167d382505c21b244142d3f2442a5d495ffa
kizzis <iswater.kim@gmail.com>
2025-03-21 00:22:38 +0900

    refactor: [order-service] product-service api 호출과 orderService에서 사용할 에러 코드 추가

--- common/src/main/java/exception/ErrorCode.java
+++ common/src/main/java/exception/ErrorCode.java
@@ -8,17 +8,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-	// 로그인 및 인증 관련 오류
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "U001"),
-	DELETED_USER(HttpStatus.BAD_REQUEST, "삭제된 사용자 정보입니다.", "U002"),
-	DUPLICATE_USERNAME(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다.", "U003"),
-	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 올바르지 않습니다.", "U004"),
-	SAME_PASSWORD_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "이전과 동일한 비밀번호로 변경할 수 없습니다.", "U005"),
-	INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증입니다.", "U006"),
-	AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다.", "U007"),
-	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.", "U008"),
-	ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.", "U009"),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", "U010"),
+    // 로그인 및 인증 관련 오류
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "U001"),
+    DELETED_USER(HttpStatus.BAD_REQUEST, "삭제된 사용자 정보입니다.", "U002"),
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다.", "U003"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 올바르지 않습니다.", "U004"),
+    SAME_PASSWORD_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "이전과 동일한 비밀번호로 변경할 수 없습니다.", "U005"),
+    INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증입니다.", "U006"),
+    AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다.", "U007"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.", "U008"),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.", "U009"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", "U010"),
 
 	// Company
 	COMPANY_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 등록되어 있는 업체입니다.", "C001"),
@@ -30,6 +30,7 @@ public enum ErrorCode {
 	// Product
 	PRODUCT_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 등록된 상품입니다.", "P001"),
 	PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않은 상품입니다.", "P002"),
+	PRODUCT_BULK_NOT_FOUND(HttpStatus.BAD_REQUEST, "상품 정보 Bulk 조회를 실패했습니다.", "P003"),
 
 	// Order
 	ORDER_NOT_FOUND(HttpStatus.BAD_REQUEST, "주문을 찾을 수 없습니다.", "O001"),
@@ -43,6 +44,10 @@ public enum ErrorCode {
 	ORDER_UPDATE_ROLE_DENIED(HttpStatus.FORBIDDEN, "권한이 MASTER 또는 HUB인 사용자만 주문을 수정할 수 있습니다.", "O009"),
 	ORDER_DELETE_ROLE_DENIED(HttpStatus.FORBIDDEN, "권한이 MASTER 또는 HUB인 사용자만 주문을 삭제할 수 있습니다.", "O010"),
 	ORDER_CANCEL_ROLE_DENIED(HttpStatus.FORBIDDEN, "권한이 MASTER 또는 HUB인 사용자만 주문을 취소할 수 있습니다.", "O011"),
+	ORDER_PRODUCT_DECREASE_STOCK_FAIL(HttpStatus.BAD_REQUEST, "OrderService에서 상품 재고 차감 API 호출을 실패하였습니다.", "O012"),
+	ORDER_ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "주문 상품 목록을 찾을 수 없습니다.", "O013"),
+	ORDER_COMPANY_GET_API_FAIL(HttpStatus.BAD_REQUEST, "OrderService에서 업체 조회 API 호출을 실패하였습니다.", "O014"),
+	ORDER_PRODUCT_RESTORE_STOCK_FAIL(HttpStatus.BAD_REQUEST, "OrderService에서 상품 재고 복원 API 호출을 실패하였습니다", "O015"),
 
 	// 배달 서비스
 	DELIVERY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 배달을 찾을 수 없습니다.", "D001"),
