74143242066408a345d6d60ae53d711a8fcf649a
kizzis <iswater.kim@gmail.com>
2025-03-21 00:23:47 +0900

    refactor: [order-service] 주문 조회 응답 dto, 주문 생성 요청 dto 필드 변경
    [주문 조회]
    - 주문자 ID, 배송 ID 추가
    - 상품명, 상품 가격 추가
    
    [주문 생성]
    - 배송 마감기한 필드 추가

--- order-service/src/main/java/com/eigtheenthstreet/order_service/application/dto/SelectOrderResponse.java
+++ order-service/src/main/java/com/eigtheenthstreet/order_service/application/dto/SelectOrderResponse.java
@@ -6,16 +6,17 @@ import java.util.UUID;
 import com.eigtheenthstreet.order_service.domain.model.Order;
 import com.eigtheenthstreet.order_service.domain.model.OrderItem;
 import com.eigtheenthstreet.order_service.domain.model.OrderStatus;
+import com.eigtheenthstreet.order_service.infrastructure.client.dto.GetBulkProductResponse;
 
 public record SelectOrderResponse(
 	UUID orderId,
-	//UUID ordererId,
+	UUID ordererId,
 	UUID supplierCompanyId,
 	UUID consumerCompanyId,
 	Integer orderTotalQuantity,
 	Integer orderTotalAmount,
 	List<SelectOrderItemResponse> orderItems,
-	//UUID deliveryId,
+	UUID deliveryId,
 	OrderStatus orderStatus
 ) {
 	public static SelectOrderResponse from(
@@ -24,32 +25,32 @@ public record SelectOrderResponse(
 	) {
 		return new SelectOrderResponse(
 			order.getId(),
+			order.getOrdererId(),
 			order.getSupplierCompanyId(),
 			order.getConsumerCompanyId(),
 			order.getQuantity(),
 			order.getTotalAmount(),
 			orderItem,
+			order.getDeliveryId(),
 			order.getOrderStatus()
 		);
 	}
 
 	public record SelectOrderItemResponse(
 		UUID productId,
+		String productName,
 		Integer productQuantity,
+		Integer productPrice,
 		Integer productTotalPrice
 	) {
-		public static SelectOrderItemResponse from(OrderItem orderItem) {
+		public static SelectOrderItemResponse from(GetBulkProductResponse productResponse, OrderItem orderItem) {
 			return new SelectOrderItemResponse(
-				orderItem.getProductId(),
+				productResponse.productId(),
+				productResponse.productName(),
 				orderItem.getQuantity(),
+				productResponse.productPrice(),
 				orderItem.getTotalPrice()
 			);
-		}
-
-		public static List<SelectOrderItemResponse> of(List<OrderItem> orderItems) {
-			return orderItems.stream()
-				.map(SelectOrderItemResponse::from)
-				.toList();
 		}
 	}
 }
