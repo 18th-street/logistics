da0256de7faee63078b121109e942dcd0a5194d1
Jong-Min <codejomo99@gmail.com>
2025-03-20 18:23:28 +0900

    feat: [Delivery-Service] Delivery-Agent 조회용 Client 및 DTO 추가

---
+++ delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/application/dto/DeliveryAgentDto.java
@@ -0,0 +1,17 @@
+package com.eighteenthstreet.deliveryservice.application.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryAgentDto {
+	private UUID deliveryAgentId;
+	private String status;
+	private int agentSequence;
+	private DeliveryRouteDto route;
+}
