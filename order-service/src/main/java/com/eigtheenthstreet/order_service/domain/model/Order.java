cf2701d516d62613077bf3ae4d7f57fff127188d
kizzis <iswater.kim@gmail.com>
2025-03-21 00:07:17 +0900

    refactor: [order-service] Order & OrderItem 엔티티 수정
    [Order]
    - Order에 목적지, 요청사항, 배송 마감기한, orderItem 연관관계 설정
    - 주문 상품의 총 금액과 총 수량을 계산하는 updateOrderTotal 메소드 수정
    - 배송 ID 저장하는 updateDeliveryId 메소드 추가
    
    [OrderItem]
    - 주문 상품의 총 수량과 총 금액 계산하는 메소드 수정

--- order-service/src/main/java/com/eigtheenthstreet/order_service/domain/model/Order.java
+++ order-service/src/main/java/com/eigtheenthstreet/order_service/domain/model/Order.java
@@ -1,5 +1,7 @@
 package com.eigtheenthstreet.order_service.domain.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,6 +9,7 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 import com.eigtheenthstreet.order_service.presentation.request.CreateOrderRequest;
+import com.eigtheenthstreet.order_service.util.DateTimeUtil;
 
 import base.BaseEntity;
 import jakarta.persistence.Column;
@@ -16,6 +19,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -43,9 +47,6 @@ public class Order extends BaseEntity {
 	@Column(name = "order_totalPrice")
 	private Integer totalAmount;
 
-	@Column(name = "order_request_details")
-	private String requestDetails;
-
 	@Enumerated(EnumType.STRING)
 	private OrderStatus orderStatus;
 
@@ -58,17 +59,46 @@ public class Order extends BaseEntity {
 	@Column(name = "supplier_company_id")
 	private UUID supplierCompanyId;
 
-	@Column(name = "consumer_company_id")
+	@Column(name = "receiver_company_id")
 	private UUID consumerCompanyId;
 
-	public static Order create(CreateOrderRequest request, Long userId) {
+	@Column(name = "order_destination_address")
+	private String destinationAddress;
+
+	@Column(name = "order_request_details")
+	private String requestDetails;
+
+	@Column(name = "order_delivery_limited_at")
+	private LocalDateTime deliveryLimitedAt;
+
+	@Builder.Default
+	@OneToMany(mappedBy = "orderId")
+	private List<OrderItem> orderItems = new ArrayList<>();
+
+	public static Order create(CreateOrderRequest request, UUID userId) {
 		return Order.builder()
 			.supplierCompanyId(request.supplierCompanyId())
 			.consumerCompanyId(request.consumerCompanyId())
-			.requestDetails(request.requestDetails())
 			.orderStatus(OrderStatus.CREATED)
-			//.ordererId(userId)
+			.ordererId(userId)
+			.requestDetails(request.requestDetails())
+			.destinationAddress(request.deliveryAddress())
+			.deliveryLimitedAt(DateTimeUtil.parseDateTime(request.deliveryLimitedAt()))
 			.build();
+	}
+
+	public void updateDeliveryId(UUID deliveryId) {
+		this.deliveryId = deliveryId;
+	}
+
+	public void updateOrderTotal(List<OrderItem> orderItems) {
+		this.quantity = orderItems.stream()
+			.mapToInt(OrderItem::getQuantity)
+			.sum();
+
+		this.totalAmount = orderItems.stream()
+			.mapToInt(OrderItem::getTotalPrice)
+			.sum();
 	}
 
 	// public void addDelivery(UUID deliveryId) {
@@ -79,20 +109,7 @@ public class Order extends BaseEntity {
 	// 	this.orderStatus = OrderStatus.DELIVERY_FAILED;
 	// }
 
-	public void addOrderTotalQuantityAndTotalAmount(int totalQuantity, Integer totalAmount) {
-		this.quantity = totalQuantity;
-		this.totalAmount = totalAmount;
-	}
-
-	public void updateOrderQuantityAndTotalPrice(List<OrderItem> updatedOrderItems) {
-		int totalQuantity = updatedOrderItems.stream()
-			.mapToInt(OrderItem::getQuantity)
-			.sum();
-
-		int totalAmount = updatedOrderItems.stream()
-			.mapToInt(OrderItem::getTotalPrice)
-			.sum();
-
+	public void saveOrderTotalQuantityAndTotalAmount(int totalQuantity, int totalAmount) {
 		this.quantity = totalQuantity;
 		this.totalAmount = totalAmount;
 	}
