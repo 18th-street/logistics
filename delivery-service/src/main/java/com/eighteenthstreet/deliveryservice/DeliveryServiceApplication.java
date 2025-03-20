be9b2b4deda0062dc17de1d8acf0c3ea26378e94
Jong-Min <codejomo99@gmail.com>
2025-03-20 16:20:22 +0900

    feat: [Delivery-Service] Feign 클라이언트 설정 및 Eureka 인식 설정 추가

--- delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/eighteenthstreet/deliveryservice/DeliveryServiceApplication.java
@@ -2,14 +2,16 @@ package com.eighteenthstreet.deliveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = {"exception", "base", "com.eighteenthstreet.deliveryservice"})
 @EnableJpaAuditing
+@EnableFeignClients
 public class DeliveryServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DeliveryServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceApplication.class, args);
+	}
 
 }
