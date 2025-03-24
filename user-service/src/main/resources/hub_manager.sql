69818da6996c01e20667501b8ab6e97ea936ce54
Yeonnnny <kdoy030@gmail.com>
2025-03-24 11:41:50 +0900

    chore: hub_manager 쿼리문 (17명)

---
+++ user-service/src/main/resources/hub_manager.sql
@@ -0,0 +1,69 @@
+INSERT INTO p_user (user_id, created_at, created_by, deleted_at, is_deleted, modified_at, modified_by,
+                    email, name, password, phone, role, slack_id, status, username)
+VALUES ('550e8400-e29b-41d4-a716-446655440001', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자1', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01012345678', 'HUB', 'slack_hubman1', 'COMPLETE', 'hubman1'),
+
+       ('550e8400-e29b-41d4-a716-446655440002', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자2', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01023456789', 'HUB', 'slack_hubman2', 'COMPLETE', 'hubman2'),
+
+       ('550e8400-e29b-41d4-a716-446655440003', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자3', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01034567890', 'HUB', 'slack_hubman3', 'COMPLETE', 'hubman3'),
+
+       ('550e8400-e29b-41d4-a716-446655440004', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자4', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01045678901', 'HUB', 'slack_hubman4', 'COMPLETE', 'hubman4'),
+
+       ('550e8400-e29b-41d4-a716-446655440005', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자5', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01056789012', 'HUB', 'slack_hubman5', 'COMPLETE', 'hubman5'),
+
+       ('550e8400-e29b-41d4-a716-446655440006', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자6', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01067890123', 'HUB', 'slack_hubman6', 'COMPLETE', 'hubman6'),
+
+       ('550e8400-e29b-41d4-a716-446655440007', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자7', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01078901234', 'HUB', 'slack_hubman7', 'COMPLETE', 'hubman7'),
+
+       ('550e8400-e29b-41d4-a716-446655440008', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자8', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01089012345', 'HUB', 'slack_hubman8', 'COMPLETE', 'hubman8'),
+
+       ('550e8400-e29b-41d4-a716-446655440009', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자9', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01090123456', 'HUB', 'slack_hubman9', 'COMPLETE', 'hubman9'),
+
+       ('550e8400-e29b-41d4-a716-446655440010', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자10', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01001234567', 'HUB', 'slack_hubman10', 'COMPLETE', 'hubman10'),
+
+       ('550e8400-e29b-41d4-a716-446655440011', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자11', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01098765432', 'HUB', 'slack_hubman11', 'COMPLETE', 'hubman11'),
+
+       ('550e8400-e29b-41d4-a716-446655440012', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자12', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01087654321', 'HUB', 'slack_hubman12', 'COMPLETE', 'hubman12'),
+
+       ('550e8400-e29b-41d4-a716-446655440013', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자13', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01076543210', 'HUB', 'slack_hubman13', 'COMPLETE', 'hubman13'),
+
+       ('550e8400-e29b-41d4-a716-446655440014', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자14', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01065432109', 'HUB', 'slack_hubman14', 'COMPLETE', 'hubman14'),
+
+       ('550e8400-e29b-41d4-a716-446655440015', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy030@gmail.com', '허브관리자15', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01054321098', 'HUB', 'slack_hubman15', 'COMPLETE', 'hubman15'),
+
+       ('550e8400-e29b-41d4-a716-446655440016', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자16', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01043210987', 'HUB', 'slack_hubman16', 'COMPLETE', 'hubman16'),
+
+       ('550e8400-e29b-41d4-a716-446655440017', NOW(), NULL, NULL, FALSE, NOW(), NULL,
+        'kdoy0388@gmail.com', '허브관리자17', '$2a$10$kBq/De7O7Bl5nYHhLT1LkOwgjw57zOiQcE5mzyhhn5qBLWcOYk9Wq',
+        '01032109876', 'HUB', 'slack_hubman17', 'COMPLETE', 'hubman17');
