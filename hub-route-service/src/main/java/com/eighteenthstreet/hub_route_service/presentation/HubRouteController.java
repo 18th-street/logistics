ce85648e4bac6a99d2e1bd1029566b8aea2393c8
dominic.yoon <142984862+dominic-yoon@users.noreply.github.com>
2025-03-25 11:33:51 +0900

    feat: swagger로 사용하여 API Docs 생성 (#51)

--- hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/presentation/HubRouteController.java
+++ hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/presentation/HubRouteController.java
@@ -17,15 +17,19 @@ import com.eighteenthstreet.hub_route_service.application.HubRouteService;
 import com.eighteenthstreet.hub_route_service.application.dto.CreateHubRouteRequest;
 import com.eighteenthstreet.hub_route_service.application.dto.GetHubRouteResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/routes")
 @RequiredArgsConstructor
+@Tag(name = "Hub Route API", description = "허브 경로 관련 API")
 public class HubRouteController {
 
 	private final HubRouteService hubRouteService;
 
+	@Operation(summary = "허브 간 최적 경로 조회", description = "출발 허브 ID와 도착 허브 ID를 기반으로 최적의 허브 경로를 반환합니다.")
 	@GetMapping()
 	public ResponseEntity<Map<String, List<GetHubRouteResponse>>> findOptimalRoute(@RequestParam UUID departureHubId,
 		@RequestParam UUID arrivalHubId) {
@@ -38,6 +42,7 @@ public class HubRouteController {
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
+	@Operation(summary = "허브 경로 추가", description = "허브 간 이동 경로를 추가합니다.")
 	@PostMapping("/add")
 	public ResponseEntity<GetHubRouteResponse> addHubRoute(@RequestBody CreateHubRouteRequest request) {
 		GetHubRouteResponse response = hubRouteService.addHubRoute(request);
