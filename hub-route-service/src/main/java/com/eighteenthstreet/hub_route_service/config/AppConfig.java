9d24d4b1e85c00c315c59c01d0a31b1bfa68e666
808dominico@naver.com <808dominico@naver.com>
2025-03-25 14:41:20 +0900

    refactor: config에서 빈 이름이 중복된 오류 해결

--- hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/config/AppConfig.java
+++ hub-route-service/src/main/java/com/eighteenthstreet/hub_route_service/config/AppConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+@Configuration("hubRouteAppConfig")
 public class AppConfig {
 
-	@Bean
+	@Bean(name = "hubRouteRestTemplate")
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
