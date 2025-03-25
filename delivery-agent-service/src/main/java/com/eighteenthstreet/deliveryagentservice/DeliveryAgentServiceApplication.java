e97cd5aebc7fce54d6e225a0f0ff75f119f4452f
Jong-Min <codejomo99@gmail.com>
2025-03-25 12:35:38 +0900

    feat: [Delivery] 공통 모듈 사용 추가

--- delivery-agent-service/src/main/java/com/eighteenthstreet/deliveryagentservice/DeliveryAgentServiceApplication.java
+++ delivery-agent-service/src/main/java/com/eighteenthstreet/deliveryagentservice/DeliveryAgentServiceApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = {"exception", "base", "auth", "com.eighteenthstreet.deliveryagentservice"})
+@SpringBootApplication(scanBasePackages = {"exception", "base", "auth", "config",
+	"com.eighteenthstreet.deliveryagentservice"})
 @EnableJpaAuditing
 @EnableFeignClients
 public class DeliveryAgentServiceApplication {
